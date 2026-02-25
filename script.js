e2dd2e961137f42fbdf0bea28b906cee9a5ca511
Francisco Rodrigues <kikocr2005@gmail.com>
2026-02-25 21:28:37 +0000

    Inicio do Projeto

---
+++ script.js
@@ -0,0 +1,21 @@
+const input_matricula_EL=document.getElementById("Matricula")
+const Vagas_Disponieis_EL=document.getElementById("VagasDisponiveis")
+
+function Send_Form(event){
+    event.preventDefault()  //Evita que a pagina dê refresh
+    console.log(input_matricula_EL.value)  //Verificar valor do input na consola
+    if(input_matricula_EL.value==""){   //value so existe em inputs
+        alert("Insira uma matricula válida!")
+    }
+    else{
+        if(Vagas_Disponieis_EL.innerText>=1){
+            var valor_atual=Vagas_Disponieis_EL.innerText
+            valor_atual=valor_atual -1
+            Vagas_Disponieis_EL.innerText=valor_atual
+        }
+        else{
+            alert("Não há mais Lugares Disponiveis")
+        }
+    }
+}
+
