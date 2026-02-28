70ff38a9c9b41994043f51f644065da91158a9aa
Francisco Rodrigues <kikocr2005@gmail.com>
2026-02-28 19:44:22 +0000

    Atualiza tabela de histórico com novos cabeçalhos e implementa funcionalidade de remoção de veículos

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
 const input_matricula_EL=document.getElementById("Matricula")
 const Vagas_Disponiveis_EL=document.getElementById("VagasDisponiveis")
 const tabela_garagem=document.getElementById("Tabela")
+
+function Registar_Entrada(){
+     //Criar Button Remover
+    const btn_RemoverVeiculo=document.createElement("button")
+    btn_RemoverVeiculo.className="btn_RemoverVeiculo"
+    btn_RemoverVeiculo.textContent="Remover Veiculo"
+
+
+    btn_RemoverVeiculo.addEventListener("click", function(event) {
+    // "this" refere-se ao botão clicado
+    const linha = this.closest("tr"); // Encontrar a linha (tr) pai do botão
+    linha.remove(); // Remover a linha
+    const vagas_atuais=parseInt(Vagas_Disponiveis_EL.innerText) //Usamos Parseint porque o + serve para concatenar texto
+    Vagas_Disponiveis_EL.innerText= vagas_atuais +1
+    });
+
+    //Tabela
+    //Cria as linhas e colunas
+    const novaLinha = document.createElement('tr') //tr  linha da tabela
+    const td_matricula=document.createElement('td') //td  coluna da tabela
+    const td_hora=document.createElement('td')
+    const acao=document.createElement ('td')
+    acao.appendChild(btn_RemoverVeiculo)
+
+    //Coloca os valores nas colunas
+    td_matricula.textContent = input_matricula_EL.value; //Pega o valor do input e coloca na tabela
+    td_hora.textContent ="Hora de entrada: " + new Date().toLocaleTimeString(); // Pega a hora atual
+
+    //Adiciona a linha com os valores a tabela
+    novaLinha.appendChild(td_matricula); //Adiciona a coluna da matricula à linha
+    novaLinha.appendChild(td_hora); //Adiciona a coluna da hora à linha
+    novaLinha.appendChild(acao);
+    tabela_garagem.appendChild(novaLinha);
+
+    input_matricula_EL.value="";  //Apaga o conteudo do input para uma nova matricula
+}
+
+
 function Send_Form(event){
     event.preventDefault()  //Evita que a pagina dê refresh
     console.log(input_matricula_EL.value)  //Verificar valor do input na consola
@@ -11,35 +49,11 @@ function Send_Form(event){
         if(Vagas_Disponiveis_EL.innerText>=1){
             event.preventDefault()
             Vagas_Disponiveis_EL.innerText=Vagas_Disponiveis_EL.innerText-1
-
-            //Criar Button Remover
-            const btn_RemoverVeiculo=document.createElement("button")
-            btn_RemoverVeiculo.id="btn_RemoverVeiculo"
-            btn_RemoverVeiculo.textContent="Remover Veiculo"
-
-
-            //Tabela
-            //Cria as linhas e colunas
-            const novaLinha = document.createElement('tr') //tr  linha da tabela
-            const td_matricula=document.createElement('td') //td  coluna da tabela
-            const td_hora=document.createElement('td')
-            const acao=document.createElement ('td')
-            acao.appendChild(btn_RemoverVeiculo)
-
-            //Coloca os valores nas colunas
-            td_matricula.textContent = input_matricula_EL.value; //Pega o valor do input e coloca na tabela
-            td_hora.textContent ="Hora de entrada: " + new Date().toLocaleTimeString(); // Pega a hora atual
-
-            //Adiciona a linha com os valores a tabela
-            novaLinha.appendChild(td_matricula); //Adiciona a coluna da matricula à linha
-            novaLinha.appendChild(td_hora); //Adiciona a coluna da hora à linha
-            novaLinha.appendChild(acao);
-            tabela_garagem.appendChild(novaLinha);
-
-            input_matricula_EL.value="";  //Apaga o conteudo do input para uma nova matricula
+            Registar_Entrada()
         }
         else{
             alert("Não há mais Lugares Disponiveis")
         }
     }
+    
 }
